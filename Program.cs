0001c3247227f748c6724cf76cfe576b7610a6e3
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-05 15:55:25 +0500

    Vazifa: Project Upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Vazifa
+{
+    class Program
+    {
+        static void Main(string[] args){
+            System.Console.Write("Biror so'z kiriting: ");
+            string str = Console.ReadLine();
+            System.Console.Write("Son kiriting: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x > str.Length){
+                
+                System.Console.WriteLine(str.ToUpper());
+            }
+            else{
+                System.Console.WriteLine(str.ToLower());
+            }
+        }
+    }
+}
+// (To.Upper() yoki str.Length metodlarisiz ishlanishi zarur emasmi?)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Vazifa-4.1
+
+String ustida amallar
+String tipida (string str) qiymat berilgan. Consoleda int tipidagi qiymat kiriting. Agar bu son str ning uzunligidan katta bo’lsa, str ni katta harflarga o’giring, aks holda kichik harflarga o’giring.
+Input: x=13, str=”teLefOn”
+Output: “TELEFON”
